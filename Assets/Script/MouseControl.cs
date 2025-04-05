f8bed0a23219921aad0e191ae890ebd72c50ccab
AlexJ231 <jyulin737@gmail.com>
2025-04-05 22:07:59 +0800

    Hola

---
+++ Assets/Script/MouseControl.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseControl : MonoBehaviour
+{
+    public float smoothSpeed = 5f;
+    private Camera mainCamera;
+    public LayerMask raycastLayerMask;
+
+    void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    void Update()
+    {
+        Vector3 target = GetMouseWorldPoint();
+        // Lerp toward the target using a smoothing factor.
+        transform.position = Vector3.Lerp(transform.position, target, smoothSpeed * Time.deltaTime);
+    }
+
+    Vector3 GetMouseWorldPoint()
+    {
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, raycastLayerMask))
+        {
+            return hit.point;
+        }
+        return transform.position;
+    }
+}
