64985576bd974bf45ebb634cca48944c90d29879
YYL061120 <binguobujiabin@gmail.com>
2025-04-05 08:29:57 +0800

    4.5 开始前1 Yan
    另一台电脑

---
+++ Assets/Script/UI/UIManager.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class UIManager : MonoBehaviour
+{
+    public GameManager gameManager;
+
+    public string currentSceneName;
+
+    [Header("Startscreen")]
+    public GameObject CreditPage;
+
+    private void Awake()
+    {
+        currentSceneName = SceneManager.GetActiveScene().name;
+    }
+
+    //public void SceneIdentifier()
+    //{
+    //    switch (currentSceneName)
+    //    {
+    //        case "Startscreen":
+
+    //            break;
+    //    }
+    //}
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Gameplay");
+    }
+
+    public void Credit()
+    {
+        if (CreditPage.activeSelf == false)
+            CreditPage.SetActive(true);
+        else
+            CreditPage.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
