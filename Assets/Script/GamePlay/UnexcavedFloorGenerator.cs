12975f5a1ac36c4cf8e444bff36bb45b17ff8313
AlexJ231 <jyulin737@gmail.com>
2025-04-06 22:12:06 +0800

    sssda

---
+++ Assets/Script/GamePlay/UnexcavedFloorGenerator.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnexcavedFloorGenerator : MonoBehaviour
+{
+    public GameObject UnexcavedFloors;
+    public List<GameObject> theList = new List<GameObject>();
+    public float TheYAxis;
+
+
+    
+
+}
