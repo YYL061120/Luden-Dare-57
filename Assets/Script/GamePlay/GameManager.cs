98e74dbf61cbbf2fd9f3679d37f56cc3f5c91be6
Husaria-w <husaria799@gmail.com>
2025-04-08 09:05:46 +0800

    存档

--- Assets/Script/GamePlay/GameManager.cs
+++ Assets/Script/GamePlay/GameManager.cs
@@ -38,6 +38,7 @@ public class GameManager : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.K) && TypeWriterTest.isTyping == false)
         {
             TypeWriterTest.tyt.AnotherDialogue();
-        }
+        }        
     }
+
 }
